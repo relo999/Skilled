29d5902fca0a237d84aabeb96f2a73c0e39709e6
jossepr8 <jossepr8@hotmail.com>
2016-09-07 12:57:34 +0200

    player physics interpolate and hitbox update again

--- Unity/Skilled/Assets/PrototypeScripts/PlayerHit.cs
+++ Unity/Skilled/Assets/PrototypeScripts/PlayerHit.cs
@@ -34,14 +34,18 @@ public class PlayerHit : MonoBehaviour {
     {
         if (didCollisionCheck) return;  //because multiple colliders are present on players
         if (c.collider is BoxCollider2D == false) return;
+        if (c.collider.gameObject.layer != LayerMask.NameToLayer("PlayerHitbox")) return;
        
-
+        
         PlayerMovement playerMov = gameObject.GetComponent<PlayerMovement>();
         GameObject other = c.collider.gameObject;
         Bounds thisBounds = gameObject.GetComponent<SpriteRenderer>().sprite.bounds;
         Bounds otherBounds = other.GetComponent<SpriteRenderer>().sprite.bounds;
+        //Debug.Log("1: " + (other.transform.position.y /*- otherBounds.size.y / 4f*/ >= transform.position.y + thisBounds.size.y / 2f));
+        //Debug.Log("2: " + (other.transform.position.x + otherBounds.size.x / 2f >= transform.position.x - thisBounds.size.x / 2f));
+        //Debug.Log("3: " + (other.transform.position.x - otherBounds.size.x / 2f <= transform.position.x + thisBounds.size.x / 2f));
         if (other.GetComponent<PlayerHit>() &&
-            other.transform.position.y - otherBounds.size.y / 4f >= transform.position.y + thisBounds.size.y / 2f &&   //TODO NEEDS WORK, more precise
+            other.transform.position.y  /*- otherBounds.size.y / 4f */>= transform.position.y + thisBounds.size.y / 2f &&   //TODO NEEDS WORK, more precise
             other.transform.position.x + otherBounds.size.x / 2f >= transform.position.x - thisBounds.size.x / 2f &&
             other.transform.position.x - otherBounds.size.x / 2f <= transform.position.x + thisBounds.size.x / 2f
             )
