783102514f8c57d3d6e0da7975e390e2206cdac9
jossepr8 <jossepr8@hotmail.com>
2016-09-27 14:09:32 +0200

    jump mechanics

--- Unity/Skilled/Assets/PrototypeScripts/PlayerMovement.cs
+++ Unity/Skilled/Assets/PrototypeScripts/PlayerMovement.cs
@@ -10,8 +10,12 @@ public class PlayerMovement : MonoBehaviour {
     public PlayerID playerID;
     public Controls controls = Controls.WASD;
     public float JumpForce = 250.0f;
-    public float HoldJumpDecay = 0.75f; //lower means higher/longer jumps
-    public float JumpENDForceDown = 50.0f;  //when releasing the jump key
+    public float HoldJumpForce = 300.0f;
+    public float HoldJumpMaxSec = 0.3f;
+    public float jumpGravityScale = 0.5f;
+
+    //public float HoldJumpDecay = 0.75f; //lower means higher/longer jumps
+    //public float JumpENDForceDown = 50.0f;  //when releasing the jump key
 
     public float MoveSpeed = 1.0f;
     public bool AirControl = true;
@@ -24,6 +28,7 @@ public class PlayerMovement : MonoBehaviour {
 
     bool _isJumping = false;
     float _currentJumpForce;
+    private float jumpTimer = 0.0f;
 
     public enum Controls
     {
@@ -54,20 +59,23 @@ public class PlayerMovement : MonoBehaviour {
     public void DoMovement(NetworkBase.PlayerInput input)
     {
         Debug.Log("Axis received: " + input.xAxis);
-        if(input.xAxis < 0)
+        Vector2 movement = new Vector2(0,_rigid.velocity.y);
+
+        if (input.xAxis < 0)
         {
-            _rigid.velocity = new Vector2(Vector2.left.x * MoveSpeed, _rigid.velocity.y);
+            movement.x += Vector2.left.x * MoveSpeed;
             LastMovedRight = false;
         }
         if (input.xAxis > 0)
         {
-            _rigid.velocity = new Vector2(Vector2.right.x * MoveSpeed, _rigid.velocity.y);
+            movement.x += Vector2.right.x * MoveSpeed;
             LastMovedRight = true;
         }
+        _rigid.velocity = movement;
     }
 
 	void Update () {
-        input = new NetworkBase.PlayerInput((int)playerID, InputManager.GetAxis("Horizontal", playerID),InputManager.GetButtonDown("Jump", playerID));
+        //input = new NetworkBase.PlayerInput((int)playerID, InputManager.GetAxis("Horizontal", playerID),InputManager.GetButtonDown("Jump", playerID));
 
         //return;
 
@@ -77,9 +85,15 @@ public class PlayerMovement : MonoBehaviour {
 
         //racasts checking if player is standing on a block, casts from both edges of hitbox
         RaycastHit2D hit =  Physics2D.Raycast((Vector2)transform.position - Vector2.up * 0.20f + new Vector2(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f, 0), -Vector2.up, 0.10f);
-        RaycastHit2D hit2 = Physics2D.Raycast((Vector2)transform.position - Vector2.up * 0.20f - new Vector2(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f, 0), -Vector2.up, 0.10f);
-        if ((hit.transform != null && hit.transform != transform) || (hit2.transform != null && hit2.transform != transform))
-            grounded = true;
+        //RaycastHit2D hit2 = Physics2D.Raycast((Vector2)transform.position - Vector2.up * 0.20f - new Vector2(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f, 0), -Vector2.up, 0.10f);
+        //if ((hit.transform != null && hit.transform != transform) || (hit2.transform != null && hit2.transform != transform))
+        //    grounded = true;
+        if (hit.transform != null && hit.transform != transform) grounded = true;
+        else
+        {
+            RaycastHit2D hit2 = Physics2D.Raycast((Vector2)transform.position - Vector2.up * 0.20f - new Vector2(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f, 0), -Vector2.up, 0.10f);
+            if (hit2.transform != null && hit2.transform != transform) grounded = true;
+        }
         Grounded = grounded;
 
             //Quickstop before input check
@@ -90,16 +104,18 @@ public class PlayerMovement : MonoBehaviour {
 
         if (grounded || AirControl)
         {
+            Vector2 movement = new Vector2(0, _rigid.velocity.y);
             if (Input.GetKey(controls == Controls.WASD ? KeyCode.A : KeyCode.LeftArrow)  || InputManager.GetAxis("Horizontal", playerID) < 0)
             {
-                _rigid.velocity = new Vector2(Vector2.left.x * MoveSpeed, _rigid.velocity.y);
+                movement.x += Vector2.left.x * MoveSpeed;
                 LastMovedRight = false;
             }
             if (Input.GetKey(controls == Controls.WASD ? KeyCode.D : KeyCode.RightArrow) || InputManager.GetAxis("Horizontal", playerID) > 0)
             {
-                _rigid.velocity = new Vector2(Vector2.right.x * MoveSpeed, _rigid.velocity.y);
+                movement.x += Vector2.right.x * MoveSpeed;
                 LastMovedRight = true;
             }
+            _rigid.velocity = movement;
         }
 
 
@@ -107,24 +123,36 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKeyUp(controls == Controls.WASD ? KeyCode.W : KeyCode.UpArrow) || InputManager.GetButtonUp("Jump", playerID) || (!grounded && _rigid.velocity.y < 0.1f && _rigid.velocity.y > -0.1f) )
         {
             _isJumping = false;
-            _rigid.AddForce(Vector2.down * JumpENDForceDown);
+           // _rigid.AddForce(Vector2.down * JumpENDForceDown);
+            //_rigid.gravityScale = 1.0f;
         }
         if (Input.GetKeyDown(controls == Controls.WASD ? KeyCode.W : KeyCode.UpArrow) || InputManager.GetButtonDown("Jump", playerID))
         {
             if (!_isJumping && (grounded || MultipleJumps))
             {
+                jumpTimer = HoldJumpMaxSec;
                 _isJumping = true;
-                _currentJumpForce = (Physics.gravity * _rigid.mass).magnitude * 5f;
+                //_currentJumpForce = (Physics.gravity * _rigid.mass).magnitude * 5f;
                 _rigid.AddForce(Vector2.up * JumpForce);
             }
         }
         if ((Input.GetKey(controls == Controls.WASD ? KeyCode.W : KeyCode.UpArrow) || InputManager.GetButton("Jump", playerID)) && _isJumping)
         {
+            
+             
+            if (jumpTimer > 0)
+            {
+                _rigid.gravityScale = jumpGravityScale;
+                _rigid.AddForce(Vector2.up * HoldJumpForce * Time.deltaTime);
+            }
 
-            _rigid.AddForce(Vector2.up * _currentJumpForce);
-
-            _currentJumpForce *= HoldJumpDecay;
+            //_currentJumpForce *= HoldJumpDecay;
         }
+        else
+        {
+            _rigid.gravityScale = 1.0f;
+        }
+        if (jumpTimer > 0) jumpTimer -= Time.deltaTime;
 
         CapSpeed();//do this last in update
     }
