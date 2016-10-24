017382f2e03415ec64c59016e80ef21655ed39d8
jossepr8 <jossepr8@hotmail.com>
2016-10-24 16:43:50 +0200

    multiplayer update pt1

---
+++ Unity/Skilled/Assets/GameState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState{
+
+	//game random seed
+    //player hit
+    //powerup spawned
+    //powerup picked up
+    //score
+    //
+}
