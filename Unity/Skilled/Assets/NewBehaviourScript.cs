0c9ca65e03382e0d9742dc22f3fb0ba4c63af632
Roel <relo999@live.nl>
2016-11-08 14:06:48 +0100

    huge

---
+++ Unity/Skilled/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Camera.main.RenderWithShader(Shader.Find("LinearBurn"),("1"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
